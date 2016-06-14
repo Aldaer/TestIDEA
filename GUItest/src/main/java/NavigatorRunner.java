0f0057641d292cbacba78a51eca8ee09ae6a93aa
Aldor <aldaer@yandex.ru>
2016-06-14 17:06:30 +0300

    Getting to know Swing

---
+++ GUItest/src/main/java/NavigatorRunner.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+/**
+ * Created by Aldor on 14.06.2016.
+ */
+public class NavigatorRunner {
+    public static void main(String[] args) {
+        Navigator.showAsMain();
+    }
+}
